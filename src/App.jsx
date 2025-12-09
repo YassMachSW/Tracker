b88b5e4cd1dd87b4e4d626500888cfa6186eb0b8
Yassmine <yassminemachsw@gmail.com>
2025-12-09 20:47:18 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const USERS = [
   { id: "316358514", name: "Alaa Shafie", phone: "+972502631406", password: "AlaaFa10" },
   { id: "205797673", name: "Jolian", phone: "+972523717287", password: "jolyass" },
   { id: "316358522", name: "Walaa Fakeeh", phone: "+972502631423", password: "wafj522" },
-  { id: "123", name: "yassmine", phone: "+972545317545", password: "123" },
+  { id: "208858183", name: "yassmine", phone: "+972545317545", password: "58183" },
 ];
 
 const STORAGE_KEY = "expenses_v1";
