bf4d51ac9d5398492db35a0cf94f298e7dc93e3a
Yassmine <yassminemachsw@gmail.com>
2025-12-09 17:11:10 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 // This is where you store users manually
 // id: unique id, name: display name, phone: default phone, password: password you give
 const USERS = [
-  { id: "316358514", name: "Alaa", phone: "0502631406", password: "AlaaFa10" },
+  { id: "316358514", name: "Alaa", phone: "+972502631406", password: "AlaaFa10" },
   { id: "u2", name: "Bob", phone: "0529876543", password: "secret" },
 ];
 
